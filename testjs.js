f3730894d986cb504863238201c54e5cacf1e5b0
Nikko B <ncbadoles@gmail.com>
2020-07-29 17:34:24 +0000

    Snake client files plus all of cloned multi-snake files(server)

--- testjs.js
+++ testjs.js
@@ -1,1 +1,33 @@
-console.log('hello');
+// Testing map method //
+
+//const lighthouses = ["Gibraltar Point", "Peggy's Point", "Cove Island", "Discovery Island", "Cape Scott", "Point Clark", "Kincardine"];
+
+/* let num = lighthouses.map(x => x.length)
+
+console.log(num)  */
+
+
+// Testin filter method //
+
+ const num = [33, 40, 17, 15, 25];
+
+/* let result = num.filter( (x) => x >= 18);
+
+console.log(result); */ 
+
+
+
+// Testing forEach() method ?? 
+
+let sum = 0;
+
+num.forEach((island, index, array)=>{
+
+    sum += island;    
+});
+
+console.log(sum)
+
+//console.log(result);
+
+
